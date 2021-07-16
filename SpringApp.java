2f9c588c8a1135092c89c2e7cf60217f85adff66
jungeun8 <80742358+jungeun8@users.noreply.github.com>
2021-07-16 17:54:59 +0900

    Add files via upload

---
+++ SpringApp.java
@@ -0,0 +1,29 @@
+package com.sample.app;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sample.service.ProductService;
+import com.sample.service.UserService;
+import com.sample.vo.Product;
+import com.sample.vo.User;
+
+public class SpringApp {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring/context-tx-aop.xml");
+		
+	
+		UserService userService = context.getBean(UserService.class);
+		User user = userService.getUserDetail("hong1234");
+		System.out.println(user);
+		
+		User user2 =User.builder().id("hong1512").password("zxcv1234").name("홍길똥")
+				.email("hong1512").phoneNumber("010-1234-1234").build();//
+		userService.addNewUser(user2);
+	}
+
+}
